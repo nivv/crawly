a3f57bec1764bf05d043d05f4719f7e0f5bda1cc
Albin Nilsson <albin.nilsson89@gmail.com>
2015-12-21 11:51:22 +0100

    fork

--- src/Crawler.php
+++ src/Crawler.php
@@ -1,11 +1,30 @@
 <?php
 
-namespace Nivv\Crawly;
+namespace Arachnid;
 
 use Goutte\Client as GoutteClient;
 use Guzzle\Http\Exception\CurlException;
 use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 
+/**
+ * Crawler
+ *
+ * This class will crawl all unique internal links found on a given website
+ * up to a specified maximum page depth.
+ *
+ * This library is based on the original blog post by Zeid Rashwani here:
+ *
+ * <http://zrashwani.com/simple-web-spider-php-goutte>
+ *
+ * Josh Lockhart adapted the original blog post's code (with permission)
+ * for Composer and Packagist and updated the syntax to conform with
+ * the PSR-2 coding standard.
+ *
+ * @package Crawler
+ * @author  Josh Lockhart <https://github.com/codeguy>
+ * @author  Zeid Rashwani <http://zrashwani.com>
+ * @version 1.0.4
+ */
 class Crawler
 {
     /**
@@ -28,8 +47,8 @@ class Crawler
 
     /**
      * Constructor
-     * @param string  $baseUrl  
-     * @param integer $maxDepth
+     * @param string $baseUrl
+     * @param int    $maxDepth
      */
     public function __construct($baseUrl, $maxDepth = 3)
     {
@@ -38,20 +57,24 @@ class Crawler
         $this->links = array();
     }
 
-
+    /**
+     * Initiate the crawl
+     * @param string $url
+     */
     public function traverse($url = null)
     {
         if ($url === null) {
             $url = $this->baseUrl;
-            $this->links[$url] = [
-                'links_text' => ['BASE_URL'],
+            $this->links[$url] = array(
+                'links_text' => array('BASE_URL'),
                 'absolute_url' => $url,
                 'frequency' => 1,
                 'visited' => false,
                 'external_link' => false,
-                'original_urls' => [$url]
-            ];
+                'original_urls' => array($url)
+            );
         }
+
         $this->traverseSingle($url, $this->maxDepth);
     }
 
@@ -72,19 +95,25 @@ class Crawler
     protected function traverseSingle($url, $depth)
     {
         try {
-            $client = $this->getGoutteClient();
+            $client = $this->getScrapClient();
+
             $crawler = $client->request('GET', $url);
             $statusCode = $client->getResponse()->getStatus();
+
             $hash = $this->getPathFromUrl($url);
             $this->links[$hash]['status_code'] = $statusCode;
+
             if ($statusCode === 200) {
                 $content_type = $client->getResponse()->getHeader('Content-Type');
+
                 if (strpos($content_type, 'text/html') !== false) { //traverse children in case the response in HTML document only
                     $this->extractTitleInfo($crawler, $hash);
+
                     $childLinks = array();
                     if (isset($this->links[$hash]['external_link']) === true && $this->links[$hash]['external_link'] === false) {
                         $childLinks = $this->extractLinksInfo($crawler, $hash);
                     }
+
                     $this->links[$hash]['visited'] = true;
                     $this->traverseChildren($childLinks, $depth - 1);
                 }
@@ -100,15 +129,15 @@ class Crawler
         }
     }
 
-
     /**
      * create and configure goutte client used for scraping
      * @return GoutteClient
      */
-    protected function getGoutteClient()
+    protected function getScrapClient()
     {
         $client = new GoutteClient();
         $client->followRedirects();
+
         $guzzleClient = new \GuzzleHttp\Client(array(
             'curl' => array(
                 CURLOPT_SSL_VERIFYHOST => false,
@@ -116,6 +145,7 @@ class Crawler
             ),
         ));
         $client->setClient($guzzleClient);
+
         return $client;
     }
 
@@ -129,8 +159,10 @@ class Crawler
         if ($depth === 0) {
             return;
         }
+
         foreach ($childLinks as $url => $info) {
             $hash = $this->getPathFromUrl($url);
+
             if (isset($this->links[$hash]) === false) {
                 $this->links[$hash] = $info;
             } else {
@@ -141,9 +173,11 @@ class Crawler
                     $this->links[$hash]['frequency'] = isset($this->links[$hash]['frequency']) ? $this->links[$hash]['frequency'] + $oldFrequency : 1;
                 }
             }
+
             if (isset($this->links[$hash]['visited']) === false) {
                 $this->links[$hash]['visited'] = false;
             }
+
             if (empty($url) === false && $this->links[$hash]['visited'] === false && isset($this->links[$hash]['dont_visit']) === false) {
                 $this->traverseSingle($this->normalizeLink($childLinks[$url]['absolute_url']), $depth);
             }
@@ -164,11 +198,14 @@ class Crawler
             $node_url = $node->attr('href');
             $node_url_is_crawlable = $this->checkIfCrawlable($node_url);
             $hash = $this->normalizeLink($node_url);
+
             if (isset($this->links[$hash]) === false) {
                 $childLinks[$hash]['original_urls'][$node_url] = $node_url;
                 $childLinks[$hash]['links_text'][$node_text] = $node_text;
+
                 if ($node_url_is_crawlable === true) {
                     // Ensure URL is formatted as absolute
+
                     if (preg_match("@^http(s)?@", $node_url) == false) {
                         if (strpos($node_url, '/') === 0) {
                             $parsed_url = parse_url($this->baseUrl);
@@ -179,8 +216,10 @@ class Crawler
                     } else {
                         $childLinks[$hash]['absolute_url'] = $node_url;
                     }
+
                     // Is this an external URL?
                     $childLinks[$hash]['external_link'] = $this->checkIfExternal($childLinks[$hash]['absolute_url']);
+
                     // Additional metadata
                     $childLinks[$hash]['visited'] = false;
                     $childLinks[$hash]['frequency'] = isset($childLinks[$hash]['frequency']) ? $childLinks[$hash]['frequency'] + 1 : 1;
@@ -190,10 +229,12 @@ class Crawler
                 }
             }
         });
+
         // Avoid cyclic loops with pages that link to themselves
         if (isset($childLinks[$url]) === true) {
             $childLinks[$url]['visited'] = true;
         }
+
         return $childLinks;
     }
 
@@ -207,9 +248,12 @@ class Crawler
         $crawler->filterXPath('//head//title')->each(function (DomCrawler $node) use ($url) {
             $this->links[$url]['title'] = trim($node->text());
         });
+
+
         $h1_count = $crawler->filter('h1')->count();
         $this->links[$url]['h1_count'] = $h1_count;
         $this->links[$url]['h1_contents'] = array();
+
         if ($h1_count > 0) {
             $crawler->filter('h1')->each(function (DomCrawler $node, $i) use ($url) {
                 $this->links[$url]['h1_contents'][$i] = trim($node->text());
@@ -227,6 +271,7 @@ class Crawler
         if (empty($uri) === true) {
             return false;
         }
+
         $stop_links = array(
             '@^javascript\:.*$@i',
             '@^#.*@',
@@ -234,14 +279,13 @@ class Crawler
             '@^tel\:.*@i',
             '@^fax\:.*@i',
         );
+
         foreach ($stop_links as $ptrn) {
             if (preg_match($ptrn, $uri) == true) {
                 return false;
             }
         }
-        if ($this->checkIfExternal($uri)) {
-            return false;
-        }
+
         return true;
     }
 
@@ -252,13 +296,9 @@ class Crawler
      */
     protected function checkIfExternal($url)
     {
-        //$base_url_trimmed = str_replace(array('http://', 'https://'), '', $this->baseUrl);
-        $linkParts = parse_url($url);
-        if (empty($linkParts['host']) || $linkParts['scheme'].'://'.$linkParts['host'] !== $this->baseUrl || isset($linkParts['fragment'])) {
-            return true;
-        }
+        $base_url_trimmed = str_replace(array('http://', 'https://'), '', $this->baseUrl);
 
-        return false;
+        return preg_match("@http(s)?\://$base_url_trimmed@", $url) == false;
     }
 
     /**
